74fe5c7b525758cbd95e0e819dedfc7532a190a8
BoDee Angus <nyx0hemir@gmail.com>
2021-06-01 00:00:07 -0600

    added functionality to ensure that the user is aware if there are more than 30 issues to display

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,10 +1,16 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction-asc";
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+                if(response.headers.get("Link")) {
+                    if(response.headers.get("Link")) {
+                        displayWarning(repo);
+                    }
+                }
             })
         }
         else {
@@ -38,4 +44,13 @@ var displayIssues = function(issues) {
     }
 };
 
-getRepoIssues("NYX1122/run-buddy");
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("facebook/react");
